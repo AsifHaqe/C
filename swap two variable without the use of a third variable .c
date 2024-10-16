e938cea609ee77685d46867bd88709fbeb808560
Asif Haque <153304056+AsifHaqe@users.noreply.github.com>
2024-10-16 18:59:15 +0600

    Add files via upload
    swap two variable without the use of a third variable

---
+++ swap two variable without the use of a third variable .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int a = 0, b = 0;
+
+    printf("Enter two number :");
+    scanf("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("a = %d, b = %d", a, b);
+    return 0;
+}
