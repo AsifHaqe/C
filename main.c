f34fe261cf459ae0ab05c2f854c1b52d8c7668c2
Asif Haque <153304056+AsifHaqe@users.noreply.github.com>
2025-01-28 21:07:50 +0600

    Reversing Array

--- main.c
+++ main.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//reverse array
 int main()
 {
-    int array[100];
-    int pos,n,i;
-    printf("Enter the number of that array :");
+    int array[100],array1[100];
+    int n,i;
+    printf("Enter an array number :");
     scanf("%d", &n);
-    printf("Enter the element :");
     for(i = 0; i < n; i++){
         scanf("%d", &array[i]);
     }
-    printf("Enter the position you wanna delete");
-    scanf("%d", &pos);
-    if(pos >= n+1){
-        printf("The deleting array is not possible");
-    } else{
-        for(i = pos-1;i < n; i++){
-            array[i] = array[i+1];
-        }
-        for(i = 0;i<n;i++){
-            printf("%d", array[i]);
-        }
+    for(i = 0; i < n; i++){
+        array1[i] = array[i];
     }
-    return 0;
+    for(i = 0; i < n; i++){
+        printf("%d",array[i]);
+    }
+    printf("The first array element are :");
+    for(i = 0; i < n;i++){
+        printf("%d", array[i]);
+    }
+    printf("The second array element are :");
+    for(i = 0; i < n;i++){
+        printf("%d", array1[i]);
+    }
+
+
 }
