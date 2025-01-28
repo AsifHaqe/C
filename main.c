fc2ce5ecc50308595ef65a188882978d0af4e0d5
Asif Haque <153304056+AsifHaqe@users.noreply.github.com>
2025-01-28 21:09:14 +0600

    Coping an Array into another array

--- main.c
+++ main.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-//reverse array
-int main()
-{
-    int array[100],array1[100];
-    int n,i;
-    printf("Enter an array number :");
-    scanf("%d", &n);
-    for(i = 0; i < n; i++){
-        scanf("%d", &array[i]);
+int main(){
+  int arra1[50],arry2[50],array3[50];
+  int n,i,j=0,k=0;
+  printf("Enter the element of that array:");
+  scanf("%d", &n);
+  for(i = 0; i < n;i++){
+    scanf("%d", &arra1[i]);
+  }
+  //logic
+  for(i = 0; i < n; i++){
+    if(arra1[i] % 2 == 0){
+        arry2[i] = arra1[i];
+        j++;
+    } else {
+        array3[k] = arra1[i];
+        k++;
     }
-    for(i = 0; i < n; i++){
-        array1[i] = array[i];
-    }
-    for(i = 0; i < n; i++){
-        printf("%d",array[i]);
-    }
-    printf("The first array element are :");
-    for(i = 0; i < n;i++){
-        printf("%d", array[i]);
-    }
-    printf("The second array element are :");
-    for(i = 0; i < n;i++){
-        printf("%d", array1[i]);
-    }
-
-
+  }
+  for(i = 0; i < j; i++){
+    printf("The odd number is: %d", arry2[i]);
+  }
+  for(i = 0; i < k; i++){
+    printf("%d", array3[i]);
+  }
+  return 0;
 }
